c6bf0d7036e39b7b6f8fa6cc09924805e8fc1bcb
VinayBagalakoti <vinaybagalakoti1425@gmail.com>
2024-04-01 02:54:33 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+function revealToSpan(){
+    document.querySelectorAll(".reveal")
+    .forEach(function(elem){
+        var parent=document.createElement('span');
+        var child =document.createElement('span');
+
+        parent.classList.add("parent");
+        child.classList.add("child");
+
+        child.innerHTML=elem.innerHTML;
+        parent.appendChild(child);
+
+        elem.innerHTML=""
+        elem.appendChild(parent)
+    })
+}
+
+
+function loaderAnimation(){
+    var t1=gsap.timeline();
+
+t1
+    .from(".child span",{
+      x:100,
+      delay:-.5,
+      stagger:.2,
+      duration:2,
+      ease:Power3.easeInOut  
+    })
+    .to(".parent .child",{
+    y:"-100%",
+    duration:1,
+    delay:-.5,
+    ease:Circ.easeInOut
+    })
+    .to(".loader",{
+        height:0,
+        duration:1,
+        ease:Circ.easeInOut
+    })
+    .to(".green",{
+        height:"100%",
+        top:0,
+        duration:1,
+        delay:-.8,
+        ease:Circ.easeInOut
+    })
+    .to(".green",{
+        height:"0%",
+        duration:1,
+        delay:-.5,
+        ease:Circ.easeInOut
+    })
+}
+revealToSpan();
+// loaderAnimation();
