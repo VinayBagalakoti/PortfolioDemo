ce58cb946b8ae004b1cc1c6fc608629ba0eb5096
VinayBagalakoti <vinaybagalakoti1425@gmail.com>
2024-04-21 01:27:06 +0530

    nju

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function disableScroll() {
+    document.body.classList.add('no-scroll');
+}
+
+// Function to enable scroll
+function enableScroll() {
+    document.body.classList.remove('no-scroll');
+}
 function revealToSpan(){
     document.querySelectorAll(".reveal")
     .forEach(function(elem){
@@ -14,8 +22,16 @@ function revealToSpan(){
         elem.appendChild(parent)
     })
 }
-
-
+function valueSetters(){
+    gsap.set(".nav a",{y:"-100%",opacity:0}) 
+    gsap.set(".home .parent .child",{y:"100%"})
+    gsap.set(".home .row img",{opacity:0})
+    document.querySelectorAll("#Visual>g").forEach(function(e){
+        var character=e.childNodes[1].childNodes[1]
+        character.style.strokeDasharray=character.getTotalLength()+'px';
+        character.style.strokeDashoffset=character.getTotalLength()+'px';  
+    })
+}
 function loaderAnimation(){
     var t1=gsap.timeline();
 
@@ -49,24 +65,52 @@ t1
         height:"0%",
         duration:1,
         delay:-.5,
-        ease:Circ.easeInOut
+        ease:Circ.easeInOut,
+        onComplete:function(){
+            enableScroll();
+            animateHomepage();
+        }
     })
 }
+function animateHomepage(){
+    
+    var tl=gsap.timeline();
 
+    tl.to(".nav a",{
+        y:0,
+        opacity:1,
+        stagger:.05,
+        ease:Expo.easeInOut
+    })
+    .to(".home .parent .child",{
+        y:0,
+        // opacity:1,
+        stagger:.1,
+        duration:1.5,
+        ease:Expo.easeInOut
+    })
+    .to(".home .row img ",{
+        opacity:1,
+        delay:-.5,
+        ease:Expo.easeInOu,
+        onComplete:function(){
+            animatesvg();
+        }
+    })
+}
 function animatesvg(){
-    document.querySelectorAll("#Visual>g").forEach(function(e){
-        var character=e.childNodes[1].childNodes[1]
-        character.style.strokeDasharray=character.getTotalLength()+'px';
-        character.style.strokeDashoffset=character.getTotalLength()+'px';  
+
 
         gsap.to("#Visual>g>g>path,#Visual>g>g>polyline",{
             strokeDashoffset:0,
             duration:2,
             ease:Expo.easeInOut,
-            delay:2
+            
         })
-    }
-)}
+}
+
+disableScroll();
 revealToSpan();
+valueSetters();
 loaderAnimation();
-animatesvg();
+
